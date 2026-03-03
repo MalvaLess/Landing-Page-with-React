6ba4b8b5699b56ffd56abeb965c15d909bf37995
moisesaguirre <moisesaguirre22@gmail.com>
2026-03-02 21:12:57 -0300

    cambios card y footer

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div><footer>
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <p>Copyright &copy; Your Website Name 2026</p>
+        </div>
+    </div>
+</footer></div>
+  )
+}
+
+export default Footer
