8a43069565de494126e94342d6b7b80193cc3320
moisesaguirre <moisesaguirre22@gmail.com>
2026-03-06 18:13:11 -0300

    cambios_Moises

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import Navbar from "./navbar.jsx";
-import Jumbotron from "./jumbotron.jsx";
 import Card from "./Card.jsx";
 import Footer from "./Footer.jsx";
 
 const Home = () => {
   return (
     <div className="text-center">
-      <Navbar />
-      <Jumbotron />
      <Card/>
 	 <Footer/>
 	 
